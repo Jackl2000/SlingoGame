2fa085c4659fbbbb97334575924e8422be08681b
Jackl2000 <jackhl2000@gmail.com>
2024-01-09 14:15:35 +0100

    Grid checks for slingo

--- Slingo/Assets/Scripts/GridNumbers.cs
+++ Slingo/Assets/Scripts/GridNumbers.cs
@@ -5,19 +5,35 @@ using UnityEngine;
 
 public class GridNumbers
 {
-    private int h;
-    private int v;
+    public int h { get; private set; }
+    public int v { get; private set; }
+    public bool hasBeenHit { get; private set; } = false;
     private GameObject gameObject;
+    private bool diagonal;
 
     public GridNumbers(int horizontal, int vertical, GameObject gameObject)
     {
         h = horizontal;
         v = vertical;
         this.gameObject = gameObject;
+        diagonal = CheckForDiagonal();
     }
+
+
+    private bool CheckForDiagonal()
+    {
+        if ((h == v) || (h == 5 && v == 1) || (h == 4 && v == 2) || (h == 2 && v == 4) || h == 1 && v == 5)
+        {
+            return true;
+        }
+        else return false;
+    }
+
 
     public void Hit()
     {
         gameObject.GetComponent<TextMeshProUGUI>().color = Color.green;
+        hasBeenHit = true;
+        gameObject.GetComponentInParent<GridCheck>().CheckGrid(h, v, diagonal);
     }
 }
