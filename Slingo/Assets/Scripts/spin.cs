391521f5dd5c3766bbd2a3ab00ad90b3f531cdd9
xTaig4 <73821893+xTaig4@users.noreply.github.com>
2024-01-09 16:07:04 +0100

    wildpicks() added

--- Slingo/Assets/Scripts/spin.cs
+++ Slingo/Assets/Scripts/spin.cs
@@ -2,21 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class spin : MonoBehaviour
 {
     public List<TMP_Text> slotTextList;
     public GridGeneration gridGenScript;
     public List<int> spinNumbers;
+    public Button spinButton;
     
     int rnd;
     int min = 1;
     int max = 15;
+    int wCount = 0;
+    int wildPicked = 0;
 
-
-    
     public void Spin()
     {
+        min = 1;
+        max = 16;
+
         if (spinNumbers.Count >= 5)
         {
             spinNumbers.Clear();
@@ -28,13 +34,13 @@ public class spin : MonoBehaviour
             min += 15;
             max += 15;
             
-            int wildPick = Random.Range(0, 5);
+            int wildPick = Random.Range(0, 10);
 
             if (wildPick == 0)
             {
                 spinNumbers.Add(0);
                 slotText.text = "W";
-                WildPick();
+                wCount++;
             }
             else
             {
@@ -44,27 +50,51 @@ public class spin : MonoBehaviour
             
         }
 
+        CheckMatchingNumb();
+    }
+
+    void CheckMatchingNumb()
+    {
         foreach (int spin in spinNumbers)
         {
             foreach (int gridNumber in gridGenScript.numberPositions.Keys)
             {
                 if (spin == gridNumber)
                 {
-                    gridGenScript.numberPositions[gridNumber].Hit();
                     Debug.Log("spin: " + spin + "Grid: " + gridNumber);
+                    gridGenScript.numberPositions[gridNumber].Hit();
                 }
             }
         }
-
-        min = 1;
-        max = 15;
+        
     }
 
-    public void WildPick()
+    public void WildPick(Button gridButton)
     {
+        
+        if (wildPicked < wCount)
+        {
+            wildPicked++;
+            gridButton.gameObject.GetComponent<TextMeshProUGUI>().color = Color.green;
+            Debug.Log("Wildpick clicked");
+            
+        }
 
     }
 
+    private void Update()
+    {
+        if (wildPicked == wCount)
+        {
+            wCount = 0;
+            wildPicked = 0;
+            spinButton.enabled = true;
+        }
+        else
+        {
+            spinButton.enabled = false;
+        }
+    }
 
 
 }
