d94f602f5a9f666f758ca1567aadc77c3cf6c242
Benjamin Rsamussen <151868889+DeusBenji@users.noreply.github.com>
2024-01-29 15:36:40 +0100

    Gevinst buntton added in seetings menu

--- Slingo/Assets/Scripts/UI/SettingsMenu.cs
+++ Slingo/Assets/Scripts/UI/SettingsMenu.cs
@@ -2,17 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     public GameObject settingsMenuPanel;
     public TextMeshProUGUI soundText;
     public GameObject spinsBetPanel;
+    public Button RewardButton;
+    public GameObject PossibleRewards;
+    public TextMeshProUGUI JackpotText;
+    public TextMeshProUGUI SuperJackpotText;
+
+
+    public void Start()
+    {
+        RewardButton.onClick.AddListener(ShowPanelRewards);
+    }
+
 
     public void ViewSettingsPanel()
     {
+        if (PossibleRewards == true)
+        {
+            PossibleRewards.SetActive(false);
+        }
+
+
         settingsMenuPanel.SetActive(!settingsMenuPanel.activeSelf);
         spinsBetPanel.SetActive(false);
+
     }
 
     public void Sound()
@@ -25,6 +44,13 @@ public class SettingsMenu : MonoBehaviour
         {
             soundText.text = "Sounds: On";
         }
+    }
+
+    public void ShowPanelRewards()
+    {
+
+        PossibleRewards.SetActive(true);
+
     }
 
     public void Info()
