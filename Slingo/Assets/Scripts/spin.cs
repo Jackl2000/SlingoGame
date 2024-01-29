e63531c81cb7d0bdf7939d92725f419c087a432a
xTaig4 <73821893+xTaig4@users.noreply.github.com>
2024-01-29 14:04:23 +0100

    background image enabled on numbers that can be wildpicked

--- Slingo/Assets/Scripts/spin.cs
+++ Slingo/Assets/Scripts/spin.cs
@@ -210,6 +210,7 @@ public class spin : MonoBehaviour
 
         int wildPick = UnityEngine.Random.Range(0, wildChance + 1);
 
+
         if (wildPick == 5)
         {
             wilds.Enqueue(slot);
@@ -241,9 +242,8 @@ public class spin : MonoBehaviour
             yield return new WaitForSeconds(0.5f);
             gridGeneration.numberPositions[number].Hit();
 
-            Transform goTrans;
-
-            goTrans = gridGeneration.numberPositions[number].gameObject.transform.GetChild(0).GetChild(0);
+            gridGeneration.numberPositions[number].gameObject.GetComponentInChildren<Image>().enabled = false;
+            Transform goTrans = gridGeneration.numberPositions[number].gameObject.transform.GetChild(0).GetChild(0);
             Image starImg = goTrans.GetComponentInChildren<Image>();
             starImg.color = new Color(starImg.color.r, starImg.color.g, starImg.color.b, 0.4f);
             starImgs.Add(starImg);
@@ -312,6 +312,7 @@ public class spin : MonoBehaviour
         if (wildPicks > 0)
         {
             WildTransparency(false);
+            Debug.Log("WildTransparency() in Spinner() ran___________");
             GridNumbers bestChoice = AI.BestChoice();
             if(bestChoice != null)
             {
@@ -331,6 +332,7 @@ public class spin : MonoBehaviour
             if (!gridNumbers.hasBeenHit || gridNumbers.gameObject == wildpick)
             {
                 Animator animatorObject = gridNumbers.gameObject.GetComponentInChildren<Animator>();
+                animatorObject.GetComponent<Image>().enabled = true; //starbackground image set to true
                 Image img = animatorObject.gameObject.transform.GetChild(1).GetComponent<Image>();
                 img.enabled = false;
                 img.color = new Color(img.color.r, img.color.g, img.color.b, 1);
@@ -339,6 +341,10 @@ public class spin : MonoBehaviour
         }
         if(stop)
         {
+            foreach (GridNumbers gridNumbers in gridGeneration.numberPositions.Values)
+            {
+                gridNumbers.gameObject.GetComponentInChildren<Image>().enabled = false;
+            }
             return;
         }
         foreach (GridNumbers gridNumbers in gridGeneration.numberPositions.Values)
@@ -346,6 +352,7 @@ public class spin : MonoBehaviour
             if (!gridNumbers.hasBeenHit)
             {
                 Animator animatorObject = gridNumbers.gameObject.GetComponentInChildren<Animator>();
+
                 Image img = animatorObject.gameObject.transform.GetChild(1).GetComponent<Image>();
                 img.enabled = true;
                 img.color = new Color(img.color.r, img.color.g, img.color.b, 0.3f);
@@ -378,7 +385,7 @@ public class spin : MonoBehaviour
     {
         foreach (var slotText in slotTextList)
         {
-            Debug.Log(slotText.gameObject.name + " to white");
+            //Debug.Log(slotText.gameObject.name + " to white");
             slotText.color = Color.white;
         }
         foreach(GridNumbers gridNumbers in gridGeneration.numberPositions.Values)
