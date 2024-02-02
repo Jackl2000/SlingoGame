7c3a05b13bc65d6a3206b47933dd6f4625d927ff
xTaig4 <73821893+xTaig4@users.noreply.github.com>
2024-02-02 15:28:45 +0100

    animation added. adjusted gevinst

--- Slingo/Assets/Scripts/CardGameManager.cs
+++ Slingo/Assets/Scripts/CardGameManager.cs
@@ -7,8 +7,19 @@ using UnityEngine.UI;
 
 public class CardGameManager : MonoBehaviour
 {
-    CardGameManager gameManager;
+    [Header("References")]
+    public PlayerData playerData;
+    private CardGameManager gameManager;
+    private Animator animator;
 
+    [Space(5)]
+    public GameObject LostPrefab;
+
+    [Space(5)]
+    public TextMeshProUGUI rewardText;
+
+    [Space(5)]
+    [Header("Card sprite")]
     public Sprite defualtCard;
     public Sprite goodHit;
     public Sprite badHit;
@@ -18,12 +29,13 @@ public class CardGameManager : MonoBehaviour
     private List<Image> cardImages = new List<Image>();
 
     private int rndCard;
-
+    private int gevints;
+    private GameObject LostGameObject;
 
     private void Awake()
     {
         gameManager = this.GetComponent<CardGameManager>();
-
+        animator = this.GetComponent<Animator>();
         foreach (GameObject goCard in cardGo)
         {
             cardTexts.Add(goCard.GetComponentInChildren<TextMeshProUGUI>());
@@ -71,64 +83,114 @@ public class CardGameManager : MonoBehaviour
         foreach (Image cardImage in cardImages)
         {
             cardImage.sprite = defualtCard;
+            cardImage.GetComponentInChildren<TextMeshProUGUI>().enabled = false;
             cardImage.color = Color.white;
+            cardImage.gameObject.GetComponent<Button>().enabled = true;
+
         }
+        LostGameObject.SetActive(false);
+        animator.SetBool("isBadFlip", false);
+        
+        gevints = 0;
+        rewardText.text = "Gevints: " + gevints.ToString();
+
         ShuffleCards();
     }
 
     public void FlipCard(GameObject cardObj)
     {
         TextMeshProUGUI cardNumberText = cardObj.GetComponentInChildren<TextMeshProUGUI>();
+        
 
         switch (Convert.ToInt32(cardNumberText.text))
         {
             case 1:
+                foreach (GameObject card in cardGo)
+                {
+                    card.GetComponent<Button>().enabled = false;
+                }
                 cardObj.GetComponent<Image>().color = Color.black;
-                cardNumberText.enabled = true;
-                cardNumberText.text = "(X o X)";
+                LostGameObject = cardObj.gameObject.transform.GetChild(1).gameObject;
+                LostGameObject.SetActive(true);
+                animator.SetBool("isBadFlip", true);
+
                 break;
             case 2:
                 cardObj.GetComponent<Image>().sprite = goodHit;
                 cardNumberText.enabled = true;
                 cardNumberText.text = "x2";
+                
+                gevints += Convert.ToInt32(cardNumberText.text.Substring(1));
+                rewardText.text = "Gevints: " + gevints + "kr";
+
                 break;
             case 3:
                 cardObj.GetComponent<Image>().sprite = goodHit;
                 cardNumberText.enabled = true;
                 cardNumberText.text = "x5";
+                
+                gevints += Convert.ToInt32(cardNumberText.text.Substring(1));
+                rewardText.text = "Gevints: " + gevints + "kr";
+
                 break;
             case 4:
                 cardObj.GetComponent<Image>().sprite = goodHit;
                 cardNumberText.enabled = true;
                 cardNumberText.text = "x5";
+
+                gevints += Convert.ToInt32(cardNumberText.text.Substring(1));
+                rewardText.text = "Gevints: " + gevints + "kr";
+
                 break;
             case 5:
                 cardObj.GetComponent<Image>().sprite = goodHit;
                 cardNumberText.enabled = true;
                 cardNumberText.text = "x10";
+                
+                gevints += Convert.ToInt32(cardNumberText.text.Substring(1));
+                rewardText.text = "Gevints: " + gevints + "kr";
+
                 break;
             case 6:
                 cardObj.GetComponent<Image>().sprite = goodHit;
                 cardNumberText.enabled = true;
                 cardNumberText.text = "x20";
+                
+                gevints += Convert.ToInt32(cardNumberText.text.Substring(1));
+                rewardText.text = "Gevints: " + gevints + "kr";
+
                 break;
             case 7:
                 cardObj.GetComponent<Image>().sprite = goodHit;
                 cardNumberText.enabled = true;
                 cardNumberText.text = "x30";
+                
+                gevints += Convert.ToInt32(cardNumberText.text.Substring(1));
+                rewardText.text = "Gevints: " + gevints + "kr";
+
                 break;
             case 8:
                 cardObj.GetComponent<Image>().sprite = goodHit;
                 cardNumberText.enabled = true;
                 cardNumberText.text = "x50";
+                
+                gevints += Convert.ToInt32(cardNumberText.text.Substring(1));
+                rewardText.text = "Gevints: " + gevints + "kr";
+
                 break;
             case 9:
                 cardObj.GetComponent<Image>().sprite = goodHit;
                 cardNumberText.enabled = true;
                 cardNumberText.text = "x100";
+                
+                gevints += Convert.ToInt32(cardNumberText.text.Substring(1));
+                rewardText.text = "Gevints: " + gevints + "kr";
+
                 break;
         }
     }
 
+
+ 
 }
 
