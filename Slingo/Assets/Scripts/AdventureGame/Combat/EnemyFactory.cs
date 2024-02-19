091d960d2d98f5715eae282f7ea3b9ff36cc6ad3
Jackl2000 <jackhl2000@gmail.com>
2024-02-19 16:15:57 +0100

    Added a UI border for player and enemy

--- Slingo/Assets/Scripts/AdventureGame/Combat/EnemyFactory.cs
+++ Slingo/Assets/Scripts/AdventureGame/Combat/EnemyFactory.cs
@@ -7,8 +7,10 @@ public class EnemyFactory : MonoBehaviour
 {
     public List<Sprite> EnemySprites = new List<Sprite>();
     public List<RuntimeAnimatorController> EnemyAnimator = new List<RuntimeAnimatorController>();
+    public List<Sprite> EnemyPortraits = new List<Sprite>();
     public GameObject enemyPrefab;
     public GameObject canvasParent;
+    public Image enemyBorder;
 
     public GameObject CreateEnemy(string enemyType, int level, GameObject spawnPoint)
     {
@@ -18,6 +20,7 @@ public class EnemyFactory : MonoBehaviour
             case "Goblin":
                 enemy.GetComponent<Animator>().runtimeAnimatorController = EnemyAnimator[0];
                 EnemyStats goblinStats = enemy.GetComponent<EnemyStats>();
+                enemyBorder.sprite = EnemyPortraits[0];
                 enemy.GetComponent<RectTransform>().sizeDelta = new Vector2(200, 200);
                 enemy.transform.localPosition = new Vector3(0, -40, 0);
                 goblinStats.Damage = Random.Range(level, level * 3);
@@ -27,6 +30,7 @@ public class EnemyFactory : MonoBehaviour
             case "Skeleton":
                 enemy.GetComponent<Animator>().runtimeAnimatorController = EnemyAnimator[1];
                 EnemyStats skeletonStats = enemy.GetComponent<EnemyStats>();
+                enemyBorder.sprite = EnemyPortraits[1];
                 skeletonStats.Damage = Random.Range(level, level * 2);
                 skeletonStats.Health = Random.Range(level, level * 6);
                 skeletonStats.CritChance = Random.Range(level, level * 2);
@@ -34,6 +38,7 @@ public class EnemyFactory : MonoBehaviour
             case "Mushroom":
                 enemy.GetComponent<Animator>().runtimeAnimatorController = EnemyAnimator[2];
                 EnemyStats mushroomStats = enemy.GetComponent<EnemyStats>();
+                enemyBorder.sprite = EnemyPortraits[2];
                 enemy.GetComponent<RectTransform>().sizeDelta = new Vector2(160, 250);
                 enemy.transform.localPosition = new Vector3(0, -10, 0);
                 mushroomStats.Damage = Random.Range(level, level + 3);
